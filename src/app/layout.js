be7a3c18d57f6ba5d0ab069bc5f568104b82fd91
Ori Roll <oriroll@gmail.com>
2024-03-14 09:22:22 +0200

    basic css styles in globals.css - initial responsive design - wrapper in app/page - some mock data

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="base-container">
+          <div className="base-wrapper">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
-import styles from "./homepage.module.css";
+import Link from 'next/link';
+import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>This is header</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, fugit
+        laudantium incidunt blanditiis cum sequi recusandae enim perspiciatis
+        fugiat amet assumenda, deleniti nemo laborum magnam nisi omnis maxime
+        saepe modi! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quo, fugit laudantium incidunt blanditiis cum sequi recusandae enim
+        perspiciatis fugiat amet assumenda, deleniti nemo laborum magnam nisi
+        omnis maxime saepe modi! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quo, fugit laudantium incidunt blanditiis cum sequi
+        recusandae enim perspiciatis fugiat amet assumenda, deleniti nemo
+        laborum magnam nisi omnis maxime saepe modi!
+      </Link>
+    </div>
+  );
 }
